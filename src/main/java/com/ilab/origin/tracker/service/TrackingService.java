a96419fef91dac9d84c812a354c415d218a7bf44
Origin Tracker <origintrack2018@gmail.com>
2018-05-30 09:16:50 +0530

    added more api for tracking

--- src/main/java/com/ilab/origin/tracker/service/TrackingService.java
+++ src/main/java/com/ilab/origin/tracker/service/TrackingService.java
@@ -6,12 +6,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ilab.origin.common.model.LocationTO;
 import com.ilab.origin.tracker.error.OriginException;
 import com.ilab.origin.tracker.model.TrackingData;
 import com.ilab.origin.tracker.repo.TrackingRepository;
@@ -40,25 +42,30 @@ public class TrackingService {
 	}
 	
 	@PostMapping("/save-location")	
-	public Result saveTrackingData(@RequestParam(value="qrcode") String qrcode , @RequestBody Location location) throws OriginException{		
-		System.out.println(" saving tracking location for qrcode: " + qrcode);
-		if(StringUtils.isEmpty(qrcode) || location == null)
+	public Result saveTrackingData(@RequestBody LocationTO location) throws OriginException{		
+		System.out.println(" saving tracking location for qrcode: " + location.getQrCode());
+		if(StringUtils.isEmpty(location.getQrCode()) || location.getLocation() == null)
 		{
 			throw new OriginException("qrcode or location is null");
 		}
 		
-		TrackingData trackData = trackRepo.findByQrCode(qrcode);
+		TrackingData trackData = trackRepo.findByQrCode(location.getQrCode());
 		if(trackData == null) {
 			throw new OriginException("qrcode seems to be invalid, its not recognized by the system");
 		}
 		List<Location> locationList = trackData.getLocations();
 		if(locationList == null) {
-			System.out.println("this is the 1st call for location tracking for the qr : " + qrcode);
+			System.out.println("this is the 1st call for location tracking for the qr : " + location.getQrCode());
 			locationList = new ArrayList<>();
 		}
-		locationList.add(location);
+		locationList.add(location.getLocation());
 		trackData = trackRepo.save(trackData);
 		System.out.println("tracking data is updated with location, updated record : " + trackData);
 		return new Result();
+	}
+	
+	@GetMapping("/get-by-qrcode")
+	public TrackingData getTrackingDetails(@RequestParam(value="qrcode") String qrcode){
+		return trackRepo.findByQrCode(qrcode);
 	}
 }
