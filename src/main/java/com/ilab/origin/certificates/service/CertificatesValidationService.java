6f5cdb58b34c5d795178b082ab9473105d6128ab
sinhanil <sinhanil@192.168.1.104>
2019-01-08 08:29:07 +0530

    merged changes

--- src/main/java/com/ilab/origin/certificates/service/CertificatesValidationService.java
+++ src/main/java/com/ilab/origin/certificates/service/CertificatesValidationService.java
@@ -94,12 +94,7 @@ public class CertificatesValidationService {
 			certificates.setCertificateId(student.getCertificateId());
 			certificates.setCertIssueDate(student.getCertIssueDate());
 
-			certificates.setQrCode(Certificates.CERTIFICATES_QR_PREFIX
-<<<<<<< HEAD
-					+ qrGenerator.generateQRCode(CertificatesUtils.getKeyInputForSignature(certificates)));
-=======
-					+ qrGenerator.generateQRCode(certificates.getKeyInputForSignature()));
->>>>>>> refs/remotes/origin/master
+			certificates.setQrCode(Certificates.CERTIFICATES_QR_PREFIX	+ qrGenerator.generateQRCode(CertificatesUtils.getKeyInputForSignature(certificates)));
 			System.out.println("qr code : " + certificates.getQrCode());
 
 			certList.add(certificates);
@@ -134,11 +129,7 @@ public class CertificatesValidationService {
 			certificates.setCourseName(student.getCourseName());
 
 			certificates.setQrCode(Certificates.CERTIFICATES_QR_PREFIX
-<<<<<<< HEAD
 					+ qrGenerator.generateQRCode(CertificatesUtils.getKeyInputForSignature(certificates)));
-=======
-					+ qrGenerator.generateQRCode(certificates.getKeyInputForSignature()));
->>>>>>> refs/remotes/origin/master
 			certList.add(certificates);
 		}
 
